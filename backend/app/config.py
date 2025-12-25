4c533e711ed0c9d70306d22f58a6a84ba93a74d2
Sanjib Dahal <sanjibdahal04@gmail.com>
2025-12-25 21:01:29 +0545

    some frontend changes

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Disaster Info Platform"
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/disaster_db")
-    AI_SERVICE_URL: str = os.getenv("AI_SERVICE_URL", "http://localhost:8002")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    AI_SERVICE_URL: str = os.getenv("AI_SERVICE_URL")
     
     class Config:
         env_file = ".env"
